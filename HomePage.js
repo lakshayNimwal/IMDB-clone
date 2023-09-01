79bf4c7612ddc1a64556f7d8a06263e34cfb9aad
Lakshay <1100lakshaynimwal@gmail.com>
2023-09-01 15:30:12 +0530

    adding comments to homepagejs

--- HomePage.js
+++ HomePage.js
@@ -1,5 +1,3 @@
-
-
 const api_key = "16f6c78";
 const url = " http://www.omdbapi.com/?apikey=";
 
@@ -8,15 +6,129 @@ const favButton = document.getElementById("fav-button");
 
 
 
-const singleMovie = async ()=>{
+// fetching the movies from api
+async function fetchMovies(query) {
+  const res = await fetch(`${url}${api_key}&s=${query}`);
+  const movies = await res.json();
+}
+
+
+// used to get every input from input tag
+searchInput.addEventListener("input", searchMovie);
+
+// search movie acc. to input
+async function searchMovie() {
+  const res = await fetch(`${url}${api_key}&s=${searchInput.value}`);
+  const data = await res.json();
+
+  displayMovie(data.Search);
+}
+
+
+
+// this function is used to display movies on homepage.
+function displayMovie(movies) {
+  const cardContainer = document.getElementById("cards-container");
+
+  var output = "";
+  for (var i of movies) {
+  
+    var imgUrl = "";
+    if (i.Poster != "N/A") {
+      imgUrl = i.Poster;
+    } else {
+      imgUrl = "./assets/blankPoster.jpg";
+    }
+
+    var id = i.imdbID;
+// creating the html
+    output += `<div class="card ">
+                <div class="card-poster">
+                  <a href="movie.html?id=${id}" ><img src= "${imgUrl}"  alt="Movie Poster"/></a>
+                  <a href="movie.html?id=${id}" ><h5 class="heading">Title: ${i.Title}</h5></a>
+                </div>
+                  <div class="flex card-info">
+                      <a href="movie.html?id=${id}" ><p>Year: ${i.Year}</p></a>
+                    <i class="fa-regular fa-lg fa-star" id="fav-icon" onclick= "addTofavrouitMovie('${id}', '${i.Title}')"></i>
+            </div>
+
+            </div>`;
+    // appending to card container 
+    cardContainer.innerHTML = output;
+  }
+}
+
+
+
+// adding movies to favorites
+function addTofavrouitMovie(id, title) {
+  localStorage.setItem(id, title);
+  alert("Movie Added to favrouites");
+}
+
+
+async function favrouietMoviePage() {
+  const favContainer = document.getElementById("fav-container");
+  var favOutput = "";
+  // get the size of local storage
+  let StorageSize = localStorage.length;
+
+  if (StorageSize == 0) {
+    alert("empty");
+    return;
+  }
+  // get the key stored in local storage
+  for (var i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+
+    const fetchUrl = `http://www.omdbapi.com/?i=${key}&apikey=${api_key}`;
+    const res = await fetch(fetchUrl);
+    const movie = await res.json();
+
+    var imgUrl = "";
+
+    if (movie.Poster != "N/A") {
+      imgUrl = movie.Poster;
+    } else {
+      imgUrl = "./assets/blankPoster.jpg";
+    }
+    //  creating movie list html
+    favOutput += `<div class="card ">
+                  <div class="card-poster">
+                  <a href="movie.html?id=${key}" >  <img src= "${imgUrl}"  alt="Movie Poster"/></a>
+                 
+                  </div>
+               <p class="heading">Title: ${movie.Title}</p>
+               <div class="flex card-info">
+            <p>Year: ${movie.Year}</p>
+        
+         <i class="fa-sharp fa-solid fa-trash" onClick="removeFav('${key}')"></i>
+            </div>
+
+            </div>`;
+  }
+  // appending to fav-container
+  favContainer.innerHTML = favOutput;
+}
+
+
+// function to remove from favorites
+function removeFav(id) {
+  localStorage.removeItem(id);
+  alert("Removed from Favorite");
+  window.location.reload();
+}
+
+// getting single movie details
+const singleMovie = async () => {
   // find id from url
   let urlQueryParameters = new URLSearchParams(window.location.search);
   let id = urlQueryParameters.get("id");
-  // console.log(id)
+  // fetching data with id
   const fetchUrl = `http://www.omdbapi.com/?i=${id}&apikey=${api_key}`;
   const res = await fetch(fetchUrl);
   const data = await res.json();
-  // console.log(data);
+ 
   var output = `
 
   <div class="movie-poster">
@@ -28,7 +140,8 @@ const singleMovie = async ()=>{
               <h2>${data.Title}</h2>
           </div>
           <div class="dh-rs">
-              <i class="fa-solid fa-bookmark" onClick=addTofavorites('${id}') style="cursor: pointer;"></i>
+          <i class="fa-regular fa-lg fa-star" id="fav-icon" onclick="addTofavrouitMovie('${id}', '${data.Title}')"></i>
+              
           </div>
       </div>
       <span class="italics-text"><i>${data.Year} &#x2022; ${data.Country} &#x2022; Rating - <span
@@ -50,120 +163,9 @@ const singleMovie = async ()=>{
           &thinsp; ${data.Awards}
       </p>
   </div> 
-  `
+  `;
+
+  // appending output to element having class movie-content.
   document.querySelector(".movie-content").innerHTML = output;
+};
 
-
-
-
-}
-
-
-
- async function favrouietMoviePage(){
-  // window.location.assign("favorite.html"); 
-  const favContainer = document.getElementById("fav-container");
-  var favOutput = '';
-    // get the key stored in local storage
-    for(var i=0; i<localStorage.length; i++){
-      
-      let key = localStorage.key(i);
-      // console.log(key);
-      const fetchUrl = `http://www.omdbapi.com/?i=${key}&apikey=${api_key}`;
-      const res = await fetch(fetchUrl);
-      const movie = await res.json();
-      // console.log(movie);
-      var imgUrl = "";
-      if (movie.Poster != "N/A") {
-        imgUrl = movie.Poster;
-      } else {
-        imgUrl = "./assets/blankPoster.jpg"
-      }
-  
-
-      favOutput += `<div class="card ">
-                  <div class="card-poster">
-                  <a href="movie.html?id=${key}" >  <img src= "${imgUrl}"  alt="Movie Poster"/></a>
-                 
-                  </div>
-               <h1>Title: ${movie.Title}</h1>
-               <div class="flex card-info">
-            <p>Year: ${movie.Year}</p>
-         <button  onClick="removeFav('${key}')"> remove </button>
-            
-            </div>
-
-            </div>`
-
-            
-    }
-      favContainer.innerHTML = favOutput;
-   
-}
-
-function removeFav(id){
-  // console.log(id)
-  localStorage.removeItem(id);
-  alert("Removed from Favorite");
-  window.location.reload();
-}
-
-async function fetchMovies(query) {
-  const res = await fetch(`${url}${api_key}&s=${query}`);
-  const movies = await res.json();
-  //  displayMovie(movies);
-}
-
-function addTofavrouitMovie(id, title){
-  console.log("id:" + id);
-  localStorage.setItem(id, title);
-  alert("Movie Added to favrouites");
-  // document.getElementById("fav-icon").classList.add("fav-icon");
-}
-
-// fetchMovies('mario');
-function displayMovie(movies) {
-  const cardContainer = document.getElementById("cards-container");
-
-  var output = "";
-  for (var i of movies) {
-    // console.log(i.Poster);
-    // console.log(i.Title);
-    // console.log(i.imdbID);
-    var imgUrl = "";
-    if (i.Poster != "N/A") {
-      imgUrl = i.Poster;
-    } else {
-      imgUrl = "./assets/blankPoster.jpg"
-    }
-
- 
-  var id = i.imdbID;
-
-  output += `<div class="card ">
-                  <div class="card-poster">
-                  <a href="movie.html?id=${id}" ><img src= "${imgUrl}"  alt="Movie Poster"/></a>
-                  </div>
-                  <a href="movie.html?id=${id}" ><h5>Title: ${i.Title}</h5></a>
-               <div class="flex card-info">
-               <a href="movie.html?id=${id}" ><p>Year: ${i.Year}</p></a>
-            <i class="fa-regular fa-lg fa-star" id="fav-icon" onclick= "addTofavrouitMovie('${id}', '${i.Title}')"></i>
-            </div>
-
-            </div>`
-
-            cardContainer.innerHTML = output;
-}}
-
-
-
-
-searchInput.addEventListener("input", searchMovie);
-
-async function searchMovie() {
-  const res = await fetch(`${url}${api_key}&s=${searchInput.value}`);
-  const data = await res.json();
-  
-  displayMovie(data.Search);
- 
-}
